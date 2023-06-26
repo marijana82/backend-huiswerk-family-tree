b54343837bdd5cc7b2ae2419b8049651d9597522
marijana82 <marijana.mikolcic@gmail.com>
2023-06-26 22:24:57 +0200

    created PersonTest and PetTest

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,111 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void getName() {
+        //arrange
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getMiddleName() {
+    }
+
+    @Test
+    void setMiddleName() {
+    }
+
+    @Test
+    void getLastname() {
+    }
+
+    @Test
+    void setLastname() {
+    }
+
+    @Test
+    void getSex() {
+    }
+
+    @Test
+    void setSex() {
+    }
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void setAge() {
+    }
+
+    @Test
+    void getMother() {
+    }
+
+    @Test
+    void setMother() {
+    }
+
+    @Test
+    void getFather() {
+    }
+
+    @Test
+    void setFather() {
+    }
+
+    @Test
+    void getSiblings() {
+    }
+
+    @Test
+    void setSiblings() {
+    }
+
+    @Test
+    void getChildren() {
+    }
+
+    @Test
+    void setChildren() {
+    }
+
+    @Test
+    void getPets() {
+    }
+
+    @Test
+    void setPets() {
+    }
+
+    @Test
+    void addParents() {
+    }
+
+    @Test
+    void addChildToChildren() {
+    }
+
+    @Test
+    void addChild() {
+    }
+
+    @Test
+    void addPet() {
+    }
+
+    @Test
+    void addSibling() {
+    }
+
+    @Test
+    void getGrandchildren() {
+    }
+}
